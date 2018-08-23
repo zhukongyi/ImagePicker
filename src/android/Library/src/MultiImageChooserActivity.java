30cb1b7ebf8cc94318f1d88c8fd89fe53faa9e04
zhufusheng <zhukongyi@vip.qq.com>
2018-08-23 16:48:21 +0800

    转UTF8编码

--- src/android/Library/src/MultiImageChooserActivity.java
+++ src/android/Library/src/MultiImageChooserActivity.java
@@ -197,7 +197,7 @@ public class MultiImageChooserActivity extends AppCompatActivity implements
         if (maxImages == 0 && isChecked) {
             isChecked = false;
             new AlertDialog.Builder(this)
-                    .setTitle("您只能选择 " + maxImageCount + " 张照片")
+                    .setTitle("您只能选择 " + maxImageCount + " 张照片") 
                     .setMessage("您一次最多只能选择 " + maxImageCount + " 张照片")
                     .setPositiveButton("确定", new DialogInterface.OnClickListener() {
                         public void onClick(DialogInterface dialog, int which) {
